3dac5548b97f3138f56849d14dd5baf73c0c8651
KirillBorisovich <116075864+KirillBorisovich@users.noreply.github.com>
2024-10-06 22:09:39 +0300

    feat: did homework

--- homework_09_10_24/homework_09_10_24/main.c
+++ homework_09_10_24/homework_09_10_24/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
-
+void swap(int* left, int* right) {
+	if (left != right) {
+		*left ^= *right;
+		*right ^= *left;
+		*left ^= *right;
+	}
+}
 void printArray(int array[], int lengh) {
 	for (int i = 0; i < lengh; ++i) {
 		printf("%d ", array[i]);
@@ -13,6 +20,56 @@ void fillArray(int array[], int lengh) {
 		array[i] = rand();
 	}
 }
+bool arrayComparison(int array1[], int array2[], int lenghs) {
+	for (int i = 0; i < lenghs; ++i) {
+		if (array1[i] != array2[i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// Smart QSort
+void insertSort(int array[], int first, int last) {
+	int newElement = 0, location = 0;
+	for (int i = first; i <= last; ++i) {
+		newElement = array[i]; 
+		location = i - 1;
+		while (location >= 0 && array[location] > newElement) {
+			array[location + 1] = array[location];
+			--location;
+		}
+		array[location + 1] = newElement;
+	}
+}
+void smartQSort(int array[], int first, int last) {
+	if (last - first + 1 < 10) {
+		insertSort(array, first, last);
+	}
+	else {
+		if (first < last) {
+			int supportingElement = array[first];
+			int left = first;
+			int right = last;
+			while (left <= right) {
+				while ((array[left] < supportingElement) && (left <= right)) {
+					++left;
+				}
+				while ((supportingElement < array[right]) && (left <= right)) {
+					--right;
+				}
+				if (left <= right) {
+					swap(&array[left], &array[right]);
+					++left;
+					--right;
+				}
+			}
+			smartQSort(array, first, right);
+			smartQSort(array, left, last);
+		}
+	}
+}
+
 
 // The most common element
 int theMostCommonElement(int array[], int lengh) {
@@ -26,6 +83,9 @@ int theMostCommonElement(int array[], int lengh) {
 		}
 	}
 	int* auxiliaryArray = (int*)calloc(max - min + 1, sizeof(int));
+	if (auxiliaryArray == NULL) {
+		return -1;
+	}
 	for (int i = 0; i < lengh; ++i) {
 		++auxiliaryArray[array[i] - min];
 	}
@@ -55,10 +115,10 @@ bool presenceInArray(int arrayToSearch[], int lengh, int searchNumber) {
 			return true;
 		}
 		else if (arrayToSearch[middle] > searchNumber) {
-			right -= (lengh - 1) / 2;
+			right = middle - 1;
 		}
 		else if (arrayToSearch[middle] < searchNumber) {
-			left += (lengh - 1) / 2;
+			left = middle + 1;
 		}
 	}
 	return false;
@@ -83,6 +143,7 @@ void wrapperSearch() {
 			arrayOfRandomNumbers[i] = rand();
 		}
 	}
+	smartQSort(arrayToSearch, 0, n - 1);
 	printf(" Array: ");
 	printArray(arrayToSearch, n);
 	for (int i = 0; i < k; ++i) {
@@ -93,7 +154,7 @@ void wrapperSearch() {
 			printf("\n The %d is not in the array", arrayOfRandomNumbers[i]);
 		}
 	}
-	printf("\n----------");
+	printf("\n----------"); 
 }
 
 // Tests
@@ -102,9 +163,22 @@ bool testTheMostCommonElement() {
 	return theMostCommonElement(array, 5) == -3;
 }
 bool testPresenceInArray() {
-	int array[] = { 1, 2, 3 };
-	return presenceInArray(array, 5, 1) && presenceInArray(array, 5, 2) && presenceInArray(array, 5, 3)
-		&& !presenceInArray(array, 5, 0) && !presenceInArray(array, 5, 4);
+	int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	for (int i = 0; i < 10; ++i) {
+		if (!presenceInArray(array, 10, array[i])) {
+			return false;
+		}
+	}
+	if (presenceInArray(array, 10, array[0] - 1) || presenceInArray(array, 10, array[9] + 1)) {
+		return false;
+	}
+	return true;
+}
+bool testSmartQsort() {
+	int array[] = { 6, 2, 5, 3, 1, 8, 2, 3, 5, 7 };
+	int referenceArray[] = { 1, 2, 2, 3, 3, 5, 5, 6, 7 ,8 };
+	smartQSort(array, 0, 9);
+	return arrayComparison(array, referenceArray, 10);
 }
 bool testProgram() {
 	return testTheMostCommonElement() && testPresenceInArray();
